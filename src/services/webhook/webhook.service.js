e0046b44b7e91d611c091c2216c7d3216a2ecb07
Samson SSali <jitpomi@gmail.com>
2020-04-20 05:46:58 -0700

    df webhook middleware

--- src/services/webhook/webhook.service.js
+++ src/services/webhook/webhook.service.js
@@ -1,14 +1,20 @@
 // Initializes the `webhook` service on path `/webhook`
 const { Webhook } = require('./webhook.class');
 const hooks = require('./webhook.hooks');
+const df=require('../../middleware');
 
-module.exports = function (app) {
+
+
+module.exports = function async (app) {
   const options = {
     paginate: app.get('paginate')
   };
 
+
+
+
   // Initialize our service with any options it requires
-  app.use('/webhook', new Webhook(options, app));
+  app.use('/webhook', new Webhook(options, app),df);
 
   // Get our initialized service so that we can register hooks
   const service = app.service('webhook');
